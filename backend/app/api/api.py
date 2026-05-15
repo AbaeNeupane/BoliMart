489df2483fee88ccdd5b2c0d46d0802b2947d7f2
Abae Neupane <108645361+AbaeNeuapne@users.noreply.github.com>
2026-05-15 05:13:54 -0400

    Debugging the backend and adding some modules

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,12 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import auth, users, listings, bids, payments, uploads, admin
+from app.api.endpoints import auth, users, listings
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(listings.router, prefix="/listings", tags=["listings"])
-api_router.include_router(bids.router, prefix="/bids", tags=["bids"])
-api_router.include_router(payments.router, prefix="/payments", tags=["payments"])
-api_router.include_router(uploads.router, prefix="/upload", tags=["uploads"])
-api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
